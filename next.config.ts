7b5fb8e566c8990333b75dcb9d70249c85a171c0
Alejandro <apacheco2603@gmail.com>
2026-07-31 18:32:53 -0500

    feat: integrar webpack-obfuscator para proteger y ofuscar el codigo fuente del cliente

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
+import WebpackObfuscator from "webpack-obfuscator";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new WebpackObfuscator(
+          {
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayThreshold: 0.75,
+            compact: true,
+            controlFlowFlattening: false,
+          },
+          ["**/node_modules/**"]
+        )
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
